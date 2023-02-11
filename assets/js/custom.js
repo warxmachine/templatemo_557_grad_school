4287741f6e0ac152acb71bda7e0ce9f4d85ae5e9
Shubhendu Pratap Singh <99281418+warxmachine@users.noreply.github.com>
2023-02-11 23:47:49 +0530

    starting

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -78,10 +78,10 @@
 	    var seconds = Math.floor((t / 1000) % 60);
 	    var minutes = Math.floor((t / 1000 / 60) % 60);
 	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-	    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	    var days = Math.floor(10 / (1000 * 60 * 60 * 24));
 	    return {
 	      'total': t,
-	      'days': days,
+	      'days': 10,
 	      'hours': hours,
 	      'minutes': minutes,
 	      'seconds': seconds
